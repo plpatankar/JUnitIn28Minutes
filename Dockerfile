95cea4ea1592717aa66e67ddecb9bf0eb5926bab
Prashant Patankar <prashantp@Prashants-MacBook-Pro.local>
2017-07-01 00:39:32 +0530

    Added Dockerfile as well

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM ubuntu
+ADD ./src /src
+ADD ./pom.xml /src/
+EXPOSE 8090
+
+
--- ExecuteBuild.sh
+++ ExecuteBuild.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 echo "Hello This is $0"
+# Cleanup 
+if [ `docker inspect -f {{.State.Running}} GitHubMaven` ]; then
+  docker stop GitHubMaven
+fi
+docker rm -f GitHubMaven 2>>/dev/null
+
+docker build -t githubmavenimage .
+docker run -d --name GitHubMaven -p 8090:8090 githubmavenimage
 
 
