fb449c8c46726645ea49c9f290bdc4c295ec7656
Prashant Patankar <plpatankar@Prashants-MacBook-Air.local>
2017-11-11 12:31:49 +0530

    Updated timeout to 1500

--- src/test/java/com/in28minutes/junit/helper/ArraysTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import org.junit.Test;
 
 public class ArraysTest {
-	@Test(timeout=500)
+	@Test(timeout=1500)
 	public void testPerformance() {
 		for(int  i=0;i<1000000;i++){
 			Arrays.sort(new int[]{i,i-1,i+1});
