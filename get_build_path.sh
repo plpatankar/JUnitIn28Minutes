89e52c92344a0cc58fc3e35db7ea4c9f83c15d95
plpatankar <plpatankar@gmail.com>
2017-11-13 17:09:03 +0530

    Update get_build_path.sh

--- get_build_path.sh
+++ get_build_path.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ls -t1 target/junit-example-*-SNAPSHOT.jar| head -1 > jarpath.txt
+cp /dev/null jarpath.txt
+ls -t1 target/junit-example-*-SNAPSHOT.jar| head -1 | tee -a jarpath.txt
 echo 2.23 > version.txt
