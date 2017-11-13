c56407575d3d0bd2985a15d91dad1b5723f98ef9
plpatankar <plpatankar@gmail.com>
2017-11-13 17:22:35 +0530

    Update get_build_path.sh

--- get_build_path.sh
+++ get_build_path.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-//echo "**/target/junit-example-1.0-SNAPSHOT.jar" > filepath.txt
-echo "/var/lib/jenkins/workspace/JUnitIn28Minutes01/target/junit-example-1.0-SNAPSHOT.jar" > filepath.txt
+echo "target/junit-example-1.0-SNAPSHOT.jar" > filepath.txt
 echo 2.23 > version.txt
