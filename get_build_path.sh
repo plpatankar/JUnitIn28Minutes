c7b10c4b0334bc7cb915fcaf3223215fbbef41ec
plpatankar <plpatankar@gmail.com>
2017-11-14 11:45:19 +0530

    Update get_build_path.sh

--- get_build_path.sh
+++ get_build_path.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 ls -t1 target/junit-example-*-SNAPSHOT.jar| head -1 > filepath.txt
-echo 2.23 > version.txt
+v1=`cat version.txt`;
+v1=v1+1
+echo $v1 > version.txt
+
+git config --global user.name "Prashant Patankar"
+git config --global user.email plpatankar@gmail.com
+git add version.txt
+git commit -m "Added new version in file"
+git push origin master
